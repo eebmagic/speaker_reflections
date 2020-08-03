a96150cd0253c355d7d98164d78ae630b62456d2
Ethan B <eebmagic@gmail.com>
2020-08-02 20:59:02 -0500

    added animation ability

--- script.py
+++ script.py
@@ -1,8 +1,10 @@
 import math
 from matplotlib import pyplot as plt
+from IPython.display import HTML
+import matplotlib.animation as animation
 
 def wave(x, t=0):
-    y = math.sin((x/5) + t)
+    y = math.sin((x/5) - (t/10))
     return y
 
 
@@ -28,6 +30,7 @@ def get_z(p, source, t=0):
 if __name__ == "__main__":
     # Area settings
     width, height = 100, 100
+    total_frames = 150
     source = (-40, 0)
 
     # Make points
@@ -41,16 +44,31 @@ if __name__ == "__main__":
         Y.append(y)
 
     # Make data at those points
-    Z = []
-    for y in Y:
-        row = []
-        for x in X:
-            curr = (x, y)
-            z = get_z(curr, source)
-            row.append(z)
-        Z.append(row)
+    frames = []
+    for t in range(0, total_frames):
+        Z = []
+        for y in Y:
+            row = []
+            for x in X:
+                curr = (x, y)
+                z = get_z(curr, source, t=t)
+                row.append(z)
+            Z.append(row)
+        frames.append(Z)
 
-    # Plot the data
-    plt.imshow(Z, extent=[0, 50, 0, 50], origin='center', cmap='RdBu')
-    plt.colorbar()
-    plt.show()
+    # Animation settings
+    fig, ax = plt.subplots()
+    im = ax.imshow(frames[0])
+
+    def init():
+        im.set_data(frames[0])
+        return (im,)
+
+    def animate(i):
+        data_slice = frames[i]
+        im.set_data(data_slice)
+        return (im,)
+
+    # Generate and export animation to file
+    anim = animation.FuncAnimation(fig, animate, init_func=init, frames=len(frames), interval=50, blit=True)
+    anim.save("OUTPUT.mp4")
